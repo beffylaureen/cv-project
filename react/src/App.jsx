055f34da417b95a26b12cefbdc5bd46e235548d3
B Laureen <beffylaureen@me.com>
2023-09-04 15:16:13 -0500

    commit after lunch

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import User from "./components/User";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import ContactForm from "./components/ContactForm";
 import "./App.css";
 
--- react/src/components/ContactForm.jsx
+++ react/src/components/ContactForm.jsx
@@ -1,51 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="mesage">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+
+};
 
 
-export default function ContactForm() {
-  const [formStatus, setFormStatus] = React.useState("Send")
-  const onSubmit = (e) => {
-    e.preventDefault()
-    setFormStatus("Submitting...")
-    const { name, email, message } = e.target.elements
-    let conForm = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }
-    console.log(conForm)
-  }
 
 
-  return(
-    <>
-      <div className="container mt-5">
-        <h2 className="mb-3">React Contact Form Component</h2>
-        <form onSubmit={onSubmit}>
-          <div className="mb-3">
-            <label className="form-label"  htmlFor="name">
-              Name
-            </label>
-            <input className="form-control" type="text" id="name" required />
-          </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input className="form-control" type="email" id="email" required />
-          </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="message">
-              Message
-            </label>
-            <textarea className="form=control" id="message" required />
-          </div>
-          <button className="btn btn-danger" type="submit">
-            {formStatus}
-            </button>
-          </form>
-        </div>
-    
-    </>
-  )
-}
 
+
+  export default ContactForm;
