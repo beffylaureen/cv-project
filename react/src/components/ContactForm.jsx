c00f70cbac36006e8ed906393c9610184afd350a
B Laureen <beffylaureen@me.com>
2023-09-04 13:53:25 -0500

    commit before lunch

---
+++ react/src/components/ContactForm.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+
+export default function ContactForm() {
+  const [formStatus, setFormStatus] = React.useState("Send")
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setFormStatus("Submitting...")
+    const { name, email, message } = e.target.elements
+    let conForm = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    }
+    console.log(conForm)
+  }
+
+
+  return(
+    <>
+      <div className="container mt-5">
+        <h2 className="mb-3">React Contact Form Component</h2>
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label className="form-label"  htmlFor="name">
+              Name
+            </label>
+            <input className="form-control" type="text" id="name" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input className="form-control" type="email" id="email" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="message">
+              Message
+            </label>
+            <textarea className="form=control" id="message" required />
+          </div>
+          <button className="btn btn-danger" type="submit">
+            {formStatus}
+            </button>
+          </form>
+        </div>
+    
+    </>
+  )
+}
+
---
+++ react/src/components/User.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function User(props){
+
+  console.log(props)
+
+
+  return(
+    <>
+    <h1>{props.name}</h1>
+      <div>
+        <p>
+            After a number of years in Brokerage Operations, I am persuing a new career in web development.
+        </p>
+      </div>
+
+
+    </>
+  )
+
+}
