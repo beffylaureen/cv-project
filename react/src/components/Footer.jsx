199cc9978aba896c379c975ff420299f30f9ff6a
B Laureen <beffylaureen@me.com>
2023-09-04 18:51:27 -0500

    commit after session with BCS learning

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -4,19 +4,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <nav>
-        <ul>
-          <li>        
-            <a href="https://github.com/beffylaureen">GitHub</a>          
-           </li>
-          <li>
-            <a href="https://www.linkedin.com">LinkedIn</a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/cables.and.purls">Instagram</a>
-          </li>
-        </ul>
-      </nav>
+      <p>
+        <a href="https://github.com/beffylaureen">GitHub</a>          
+      </p>
     </footer>
   );
 }
