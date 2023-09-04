c5f291301947893e832106bc5a28c6422ce48d22
B Laureen <beffylaureen@me.com>
2023-09-04 16:11:43 -0500

    1611 commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import User from "./components/User";
 import ContactForm from "./components/ContactForm";
 import "./App.css";
+import Header from "./components/Header";
+
 
 
 
 function App() {
+  const imageStyle = {
+    width: "300px",
+    height: "300x",
+  }
   
 
   return (
     <>
+      <Header />
       <User name="Beth L. Haynes" />
+      <img src="../src/assets/IMG_2048.jpg" alt="Me" style={imageStyle}/>
 
     </>
   )
--- react/src/components/User.jsx
+++ react/src/components/User.jsx
@@ -7,7 +7,7 @@ export default function User(props){
 
   return(
     <>
-    <h1>{props.name}</h1>
+   {/*<h1>{props.name}</h1>*/}
       <div>
         <p>
             After a number of years in Brokerage Operations, I am persuing a new career in web development.
